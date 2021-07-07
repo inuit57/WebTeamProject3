47a396a3a8cf793353818f40055c1361c644e038
narD <inuit57@naver.com>
2021-07-07 12:10:51 +0900

    거래 버튼 작업 완료

--- src/com/chat/action/ChatSaveAction.java
+++ src/com/chat/action/ChatSaveAction.java
@@ -19,14 +19,18 @@ public class ChatSaveAction implements Action {
 		String chatRole = request.getParameter("chatRole");
 		String msg = request.getParameter("msg");
 		
+		if("sell".equals(msg)){
+			msg = "상품을 받으신 것이 확인되셨다면 '구매확정'을 눌러주세요.";
+		}
+		
 		String saveMsg = chatRole + "|" + msg + "/";
 		
 		ServletContext ctx = request.getServletContext();
 		String realpath = ctx.getRealPath("/chatlog/");
 		
-		
-		
+		//System.out.println("chat log path :: " + realpath);
 		try {
+			// 텍스트 파일에 채팅 내용 작성해주기 
 			BufferedWriter fw = new BufferedWriter(new FileWriter(realpath + roomId + ".txt",true));
 //			BufferedWriter fw = new BufferedWriter(new FileWriter(realpath + roomId,true));
 			
